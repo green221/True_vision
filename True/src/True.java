9080d8b8344af3d6879b9622c7567fe9dc804a83
true <true@tvt42>
2019-06-25 14:06:56 +0530

    java.java

--- True/src/True.java
+++ True/src/True.java
@@ -1,4 +1,23 @@
 
-public class True {
+	import java.io.*;
 
-}
+	public class True {
+
+	public static void main(String[] args) {
+	boolean dirFlag = false;
+
+	// create File object
+	File stockDir = new File("d://Stock/ stockDir ");
+
+	try {
+	   dirFlag = stockDir.mkdir();
+	} catch (SecurityException Se) {
+	System.out.println("Error while creating directory in Java:" + Se);
+	}
+
+	if (dirFlag)
+	   System.out.println("Directory created successfully");
+	else
+	   System.out.println("Directory was not created successfully");
+	}
+	}
